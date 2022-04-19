4531581c3b2231665e2bb303fc152b2e34ef3556
Shivam Singh <shivamfaceuit@gmail.com>
2022-04-19 10:45:05 +0530

    binary srch impl

---
+++ src/SerchingAlgo/com/BinarySrch.java
@@ -0,0 +1,27 @@
+package SerchingAlgo.com;
+
+public class BinarySrch {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        int key=5;
+        int ans=srch(arr,key);
+        System.out.println(ans);
+    }
+
+      static int srch(int[] arr, int key) {
+        int low=0;
+        int high= arr.length-1;
+        int mid=(low+high)/2;
+
+        while (low<=high){
+            if (arr[mid]==key){
+                return mid;
+            }else if(arr[mid]>key){
+                high=mid-1;
+            }else if (arr[mid]<key){
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+}
