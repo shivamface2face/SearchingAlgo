f692ade8039b84d4090869f757245a771f545f49
Shivam Singh <shivamfaceuit@gmail.com>
2022-04-19 12:05:54 +0530

    count the 1s in  binary sorted array

---
+++ src/SerchingAlgo/com/CountOneinBinaryAray.java
@@ -0,0 +1,28 @@
+package SerchingAlgo.com;
+
+public class CountOneinBinaryAray {
+    public static void main(String[] args) {
+        int arr[] = {0, 0, 1, 1, 1, 1}, n = 6;
+
+
+        System.out.println(countOnes(arr, n));
+    }
+
+    static int countOnes(int[] arr, int n) {
+        int low=0;
+        int high=n-1;
+        while (low<=high){
+            int mid=(low+high)/2;
+            if (arr[mid]==0){
+                low=mid+1;
+            }else {
+                if (mid==0 || arr[mid-1]==0){
+                    return (n-mid);
+                }else {
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
